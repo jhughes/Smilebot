954db880708c0d35b93ff6d24d89eadcc56e6b31
Kiran Ryali <kiran.ryali@gmail.com>
2011-09-21 15:23:10 -0500

    adding in android

---
+++ pyandroid/src/com/pyrobot/droid/MainMenu.java
@@ -0,0 +1,21 @@
+package com.pyrobot.droid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainMenu extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+    public void launchControllerActivity(View v) {
+    	Toast.makeText(MainMenu.this, "Controller launched", Toast.LENGTH_LONG).show();
+    }
+}
